768917667bd52dfb46c08443fb4d4361f7397968
avishekataur <avishekataur@gmail.com>
2021-09-06 18:45:27 +0600

    task-two

--- task.js
+++ task.js
@@ -33,3 +33,44 @@ setTimeout(
 );
 
 console.log('third');
+
+//Task(2)
+const add = () =>
+{
+    const userInput = prompt("Give a positive number");
+    const mainResult = parseFloat(userInput) + 200;
+
+    const result = document.getElementById('result');
+    result.innerText = parseFloat(mainResult);
+    result.innerText = `Result  :   ${mainResult}`;
+
+    alert(`Result ${mainResult}`);
+       
+}
+
+const profile = () =>
+{
+    const response = confirm('Avishek, Do you want see your GitHub Profile?');
+
+    if(response == true)
+    {
+        // const address = location.assign('https://github.com/avishekataur');
+        // console.log(address);
+        // location.href = 'https://github.com/avishekataur';
+        // console.log('https://github.com/avishekataur');
+
+        window.open("https://github.com/avishekataur", "_blank");
+
+        const url = 'https://github.com/avishekataur';
+        console.log(url);
+
+        
+    }
+    else
+    {
+        console.log('No Profile');
+    }
+    
+
+    //  console.log(location.assign('https://github.com/avishekataur'));
+}
