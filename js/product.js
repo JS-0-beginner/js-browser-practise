449f5099fb31480e0478e32d9f6a6a16e66ca5a5
avishekataur <avishekataur@gmail.com>
2021-09-07 21:59:56 +0600

    li-done

--- js/product.js
+++ js/product.js
@@ -1,1 +1,26 @@
 console.log('Product & Price');
+
+const addProduct = () =>
+{
+    const product = document.getElementById('product');
+    const productText = product.value;
+    
+    const price = document.getElementById('price');
+    const priceText = price.value;
+    
+    const productList = document.getElementById('product-list');
+    const productCount = document.createElement('li');
+    productCount.innerHTML = `<li>${productText}</li>`;
+    productList.appendChild(productCount);
+
+    const priceList = document.getElementById('price-list');
+    const priceCount = document.createElement('li');
+    priceCount.innerHTML = `<li>${priceText}</li>`;
+    priceList.appendChild(priceCount);
+
+    product.value = '';
+    price.value = '';
+    
+    
+
+}
