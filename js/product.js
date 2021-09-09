73a438bbdfe91e7bd84ddc9c3fb1a187905fb3cb
avishekataur <avishekataur@gmail.com>
2021-09-09 09:12:12 +0600

    UI-done

--- js/product.js
+++ js/product.js
@@ -20,7 +20,30 @@ const addProduct = () =>
 
     product.value = '';
     price.value = '';
+      
+
+}
+
+const order = () =>
+{
+    const list = document.getElementById('the-list');
+    list.textContent = '';
     
+    const product = document.getElementById('product');
+    product.setAttribute('disabled', true);
+
+    const price = document.getElementById('price');
+    price.setAttribute('disabled', true);
+
+    const addButton = document.getElementById('add-btn');
+    addButton.setAttribute('disabled', true);
+    
+    const message = document.createElement('p');
+    message.innerHTML = `<div class="container">
+    <p>Your order has been send to the Local Storage, <strong><a href="">Reload</a></strong> the page for new order</p>
+  </div>`;
+    list.appendChild(message);
+
     
 
 }
